0218fe966547ee165f611aadb2673c7705fa028c
inwoo0808 <inwoo0808@gmail.com>
2024-11-28 12:41:04 +0900

    intent 값 전달 연동 완료

--- app/src/main/java/com/example/workoutdiary/RoutineStartActivity.kt
+++ app/src/main/java/com/example/workoutdiary/RoutineStartActivity.kt
@@ -17,8 +17,19 @@ public class RoutineStartActivity  : AppCompatActivity() {
         val setTextView = findViewById<TextView>(R.id.setTv)
         val startButton = findViewById<Button>(R.id.startButton)
 
+        val routineTitle = intent.getStringExtra("routineTitle") ?: "기본 제목"
+        val routine = intent.getStringExtra("routine") ?: "기본 루틴"
+        val count = intent.getStringExtra("count") ?: "기본 세트"
+
+        // 받은 데이터를 TextView에 설정
+        titleTextView.text = routineTitle
+        setTextView.text = count
+
         startButton.setOnClickListener {
             val intent = Intent(this, RoutineTimerActivity::class.java)
+            intent.putExtra("routineTitle", routineTitle)
+            intent.putExtra("routine", routine)
+            intent.putExtra("count", count)
             startActivity(intent)
         }
 
